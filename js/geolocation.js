e26188fc9f8798b2cf982acfafa5eaed7191f3d3
David Álvarez <david11988@gmail.com>
2016-12-14 21:14:33 +0100

    Añado ejemplo de ajax, jquery y fix para IE9

--- js/geolocation.js
+++ js/geolocation.js
@@ -7,8 +7,27 @@ function getLocation(){
 }
 
 function showPosition(position) {
-	console.log(position);
+	var url = "http://maps.googleapis.com/maps/api/geocode/json?latlng=" + position.coords.latitude + "," + position.coords.longitude + "&sensor=true";
+	var xhr;
+
+	if (window.XMLHttpRequest) {
+		xhr = new XMLHttpRequest();
+	} else {
+		xhr = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+
+	xhr.open("GET", url, true);
+
+	xhr.onreadystatechange = function () {
+		if (xhr.readyState == 4) {
+			console.log(xhr);
+			console.log("Respuesta servidor", JSON.parse(xhr.responseText));
+		}
+	}
+
+	xhr.send();
 }
+
 
 function errorHandler(error) {
 	switch(error.code) {
@@ -27,4 +46,30 @@ function errorHandler(error) {
     }
 }
 
-//getLocation();
+
+function fakePost() {
+	var url = 'http://jsonplaceholder.typicode.com/posts';
+
+	var xhr;
+
+	if (window.XMLHttpRequest) {
+		xhr = new XMLHttpRequest();
+	} else {
+		xhr = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+
+	xhr.open("GET", url, true);
+
+	xhr.onreadystatechange = function () {
+		if (xhr.readyState == 4) {
+			console.log(xhr);
+			console.log("Respuesta servidor", JSON.parse(xhr.responseText));
+		}
+	}
+
+	xhr.send();
+}
+
+// fakePost();
+
+getLocation();
